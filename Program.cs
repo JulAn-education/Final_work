95bb1c8804a6c7dae6d052f10f455e9d10d40073
«Julia» <jul_an@mail.ru>
2023-01-29 14:36:13 +0300

    Доавлено решение задачи

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Input a word or symbols: ");
+string word1 = Console.ReadLine();
+Console.Write("Input a word or symbols: ");
+string word2= Console.ReadLine();
+Console.Write("Input a word or symbols: ");
+string word3 = Console.ReadLine();
+Console.Write("Input a word or symbols: ");
+string word4 = Console.ReadLine();
+
+string[] array = new string[] {word1, word2, word3, word4};
+string[] newarray = new string[array.Length];
+
+
+void SortingArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarray[count] = array[i];
+            count++;
+        } 
+    }
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SortingArray(array, newarray);
+ShowArray(newarray);
+
+
